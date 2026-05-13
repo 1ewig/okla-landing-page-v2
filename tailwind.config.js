bea0e781f31033f8be682247df016c09ed853f66
Moshu <sellfor59@gmail.com>
2026-05-13 16:24:50 +0500

    chore: expand marquee text details and slow it down to 70s

--- tailwind.config.js
+++ tailwind.config.js
@@ -87,7 +87,7 @@ module.exports = {
         },
       },
       animation: {
-        "marquee": "marquee 40s linear infinite",
+        "marquee": "marquee 70s linear infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
